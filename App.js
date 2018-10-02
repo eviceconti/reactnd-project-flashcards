abf83def0173caa88b9bb3d2f07971edb8d39136
Enrico Viceconti <dev.eviceconti@gmail.com>
2018-10-02 09:34:29 -0300

    tabs inserted

--- App.js
+++ App.js
@@ -1,12 +1,37 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator, BottomTabBar } from 'react-navigation'
+
+const Decks = () => {
+	return (
+		<View>
+      <Text>decks!</Text>
+    </View>
+  )
+}
+
+const NewDeck = () => {
+	return (
+		<View>
+      <Text>new deck!</Text>
+    </View>
+  )
+}
+
+const Tabs = createBottomTabNavigator({
+  Decks: {
+    screen: Decks
+  },
+  NewDeck: {
+    screen: NewDeck
+  }
+});
 
 export default class App extends React.Component {
+  
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <Tabs />
     );
   }
 }
@@ -14,7 +39,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
